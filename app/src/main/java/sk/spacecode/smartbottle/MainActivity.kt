f77b220c02672e55ceaca30da88c320e3bfab0e1
Jakub Brehuv <Jakub.Brehuv1@ibm.com>
2018-10-26 15:26:54 +0200

    qr scanner activity added

--- app/src/main/java/sk/spacecode/smartbottle/MainActivity.kt
+++ app/src/main/java/sk/spacecode/smartbottle/MainActivity.kt
@@ -1,13 +1,20 @@
 package sk.spacecode.smartbottle
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val intent = Intent(this, CodeScannerActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
