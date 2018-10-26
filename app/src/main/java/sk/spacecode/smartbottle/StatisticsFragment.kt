ba6f311425900fb1aaf4ccf5e6633aa1d0779442
Dominik Stefan <stefan@localhost.company>
2018-10-26 19:06:15 +0200

    vytvorenie bottom menu a fragmenty pre main

---
+++ app/src/main/java/sk/spacecode/smartbottle/StatisticsFragment.kt
@@ -0,0 +1,53 @@
+package sk.spacecode.smartbottle
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [StatisticsFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [StatisticsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class StatisticsFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistics, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment StatisticsFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+            StatisticsFragment()
+    }
+}
