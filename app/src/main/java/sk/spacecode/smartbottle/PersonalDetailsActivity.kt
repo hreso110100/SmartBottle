6a785eaa76270bfd1ce6b53375816a41505a5435
Jakub Brehuv <Jakub.Brehuv1@ibm.com>
2018-10-27 11:51:00 +0200

    fix profile

--- app/src/main/java/sk/spacecode/smartbottle/PersonalDetailsActivity.kt
+++ app/src/main/java/sk/spacecode/smartbottle/PersonalDetailsActivity.kt
@@ -35,20 +35,16 @@ class PersonalDetailsActivity : AppCompatActivity() {
             if (name.isNotEmpty() && weight.isNotEmpty()) {
                 val personData = Person(name.toString(), weight.toString())
 
-                val dialog = indeterminateProgressDialog("Wait  a bit ...")
 
-                dialog.show()
                 mDatabase!!.child(bottleId).child("personalData").setValue(personData).addOnCompleteListener { task ->
                     when {
                         task.isSuccessful -> {
                             val intent = Intent(this, MainActivity()::class.java)
                             intent.putExtra("device_mac", bottleId)
                             startActivity(intent)
-                            dialog.hide()
                         }
                         else -> {
                             Toast.makeText(this, "Push unsucessfull.", Toast.LENGTH_SHORT).show()
-                            dialog.hide()
                         }
                     }
                 }
